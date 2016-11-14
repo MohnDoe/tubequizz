ef735f881418eecc44e7de6c0df443b0a4255bab
Kevin F. P <kfnpfeffer@gmail.com>
2016-11-14 04:04:50 +0100

    some tweaks + UI up

--- src/public/js/controller/quizz.js
+++ src/public/js/controller/quizz.js
@@ -7,78 +7,88 @@ angular.module('App')
 
         scope.clipOptions = {};
 
-        scope.clips = [{
-            title: "MUT MUT ÉCARTEZ-VOUS (Euro Truck Simulator 2)",
-            video_id: 'ADUJ87Wxyp4',
-            thumbnail: 'https://i.ytimg.com/vi/ADUJ87Wxyp4/mqdefault.jpg',
-            start_time: 181285,
-            duration: 19172
-        }, {
-            title: "ET MAINTENANT, FERME TA GUEULE (DayZ)",
-            video_id: '80jDpdh4wBc',
-            thumbnail: 'https://i.ytimg.com/vi/80jDpdh4wBc/mqdefault.jpg',
-            start_time: 466145,
-            duration: 18681
-        }, {
-            title: "J'APPRENDRE LA SURVIE ET LA CONJUGAISON (H1Z1 Battle Royale)",
-            video_id: '--6EQeJqf8E',
-            thumbnail: 'https://i.ytimg.com/vi/--6EQeJqf8E/mqdefault.jpg',
-            start_time: 450593,
-            duration: 23160
-        }, {
-            title: "J'APPRENDRE LA SURVIE ET LA CONJUGAISON (H1Z1 Battle Royale)",
-            video_id: '--6EQeJqf8E',
-            thumbnail: 'https://i.ytimg.com/vi/--6EQeJqf8E/mqdefault.jpg',
-            start_time: 435170,
-            duration: 14830
-        }, {
-            title: "ET J'AI CRAMÉ (Half-Life 2 en COOP)",
-            video_id: 'i-ELRMO3vHQ',
-            thumbnail: 'https://i.ytimg.com/vi/i-ELRMO3vHQ/mqdefault.jpg',
-            start_time: 701483,
-            duration: 5000
-        }];
+        scope.level = {
+            number: 1,
+            clips_count: 5,
+            clips_total: 500,
+            clips: [{
+                title: "MUT MUT ÉCARTEZ-VOUS (Euro Truck Simulator 2)",
+                video_id: 'ADUJ87Wxyp4',
+                thumbnail: 'https://i.ytimg.com/vi/ADUJ87Wxyp4/mqdefault.jpg',
+                start_time: 181285,
+                duration: 19172,
+                points: 300
+            }, {
+                title: "ET MAINTENANT, FERME TA GUEULE (DayZ)",
+                video_id: '80jDpdh4wBc',
+                thumbnail: 'https://i.ytimg.com/vi/80jDpdh4wBc/mqdefault.jpg',
+                start_time: 466145,
+                duration: 18681,
+                points: 300
+            }, {
+                title: "J'APPRENDRE LA SURVIE ET LA CONJUGAISON (H1Z1 Battle Royale)",
+                video_id: '--6EQeJqf8E',
+                thumbnail: 'https://i.ytimg.com/vi/--6EQeJqf8E/mqdefault.jpg',
+                start_time: 450593,
+                duration: 23160,
+                points: 300
+            }, {
+                title: "J'APPRENDRE LA SURVIE ET LA CONJUGAISON (H1Z1 Battle Royale)",
+                video_id: '--6EQeJqf8E',
+                thumbnail: 'https://i.ytimg.com/vi/--6EQeJqf8E/mqdefault.jpg',
+                start_time: 435170,
+                duration: 14830
+            }, {
+                title: "ET J'AI CRAMÉ (Half-Life 2 en COOP)",
+                video_id: 'i-ELRMO3vHQ',
+                thumbnail: 'https://i.ytimg.com/vi/i-ELRMO3vHQ/mqdefault.jpg',
+                start_time: 701483,
+                duration: 5000,
+                points: 300
+            }]
+        };
 
         scope.actualPosition = 0;
 
         scope.videos = [];
 
-        scope.answers = [];
+        scope.currentAnswers = [];
 
-        scope.answersLoaded = false;
+        scope.currentAnswersLoaded = false;
 
         scope.percentTimerLeft = 100;
         scope.potentialPoints = 300;
 
         scope.userScore = 0;
 
-        scope.popupVisible = false;
-
-        scope.popupOptions = {
-            quizz: {
-                channel: 'wankil',
-                gifs: {
-                    correct: 8,
-                    wrong: 4
+        scope.popup = {
+            visible: false,
+            options: {
+                quizz: {
+                    channel: 'wankil',
+                    gifs: {
+                        correct: 8,
+                        wrong: 4
+                    },
+                    number_levels: 10
                 }
+            },
+            infos: {
+                answer: {
+                    points: 10,
+                    is_correct: true,
+                    next: true
+                },
+                end_game: {
+
+                },
+                type: 'answer'
             }
-        }
-
-        scope.popupInfos = {
-            answer: {
-                points: 10,
-                is_correct: true,
-                next: true
-            },
-            end_game: {
-
-            },
-            type: 'answer'
         }
 
 
         scope.initClip = function(position) {
-            clip = scope.clips[position];
+            clip = scope.level.clips[position];
             scope.clipOptions = clip;
             scope.actualPosition = position;
             $rootScope.$emit('clipChanged', clip);
@@ -95,6 +105,7 @@ angular.module('App')
         scope.nextClip = function() {
             actualPosition = scope.actualPosition;
             if (actualPosition + 1 < 5) {
+                scope.currentAnswersLoaded = false;
                 scope.initClip(actualPosition + 1);
             }
         }
@@ -111,7 +122,7 @@ angular.module('App')
         }
 
         scope.deleteAnswers = function() {
-            scope.answers = [];
+            scope.currentAnswers = [];
         }
 
         scope.randomizeVideos = function() {
@@ -130,42 +141,42 @@ angular.module('App')
         }
 
         scope.getAnswers = function() {
-            scope.answersLoaded = false;
+            scope.currentAnswersLoaded = false;
             scope.randomizeVideos();
             for (var i = 0; i < 3; i++) {
-                scope.answers[i] = scope.videos[i];
+                scope.currentAnswers[i] = scope.videos[i];
             }
-            scope.answers[3] = {
+            scope.currentAnswers[3] = {
                 id: {
-                    videoId: scope.clips[scope.actualPosition].video_id
+                    videoId: scope.level.clips[scope.actualPosition].video_id
                 },
                 snippet: {
-                    title: scope.clips[scope.actualPosition].title,
+                    title: scope.level.clips[scope.actualPosition].title,
                     thumbnails: {
                         medium: {
-                            url: scope.clips[scope.actualPosition].thumbnail
+                            url: scope.level.clips[scope.actualPosition].thumbnail
                         }
                     }
                 }
             }
-            scope.answers = scope.shuffle(scope.answers);
-            scope.answersLoaded = true;
-            // console.log(scope.answers);
+            scope.currentAnswers = scope.shuffle(scope.currentAnswers);
+            scope.currentAnswersLoaded = true;
+            // console.log(scope.currentAnswers);
         }
 
         scope.answer = function(a) {
             scope.stopTimer();
 
-            current_clip_id = scope.clips[scope.actualPosition].video_id;
+            current_clip_id = scope.level.clips[scope.actualPosition].video_id;
             if (current_clip_id == a) {
                 is_correct = true;
             } else {
                 is_correct = false;
             }
-            scope.updateUserScore(is_correct);
+            gainedPoints = scope.updateUserScore(is_correct);
             $rootScope.$emit('answerGiven', {
                 is_correct: is_correct,
-                points: scope.userScore,
+                points: gainedPoints,
                 next: true
             });
             // scope.nextClip();
@@ -216,15 +227,15 @@ angular.module('App')
             var potentialPoints = scope.calculatePotentialPoints();
             return {
                 potentialPoints: potentialPoints,
-                wrongPoints: scope.isTimeUp ? -(300 / 2) : Math.round(-Math.abs(potentialPoints) / 1.5)
+                wrongPoints: scope.isTimeUp ? -(scope.level.clips[scope.actualPosition].points / 2) : Math.round(-Math.abs(potentialPoints) / 1.5)
             }
         }
 
         scope.getChoiceDuraton = function() {
-            var numberOfLevels = 10; //TODO : get number of levels
+            var numberOfLevels = scope.popup.options.quizz.number_levels;
             var minChoiceDuration = 10 * 10; //10 sec
             var maxChoiceDuration = 30 * 10; // 30 sec
-            var currentLevel = 10; // TODO : get current level
+            var currentLevel = scope.level.number; // TODO : get current level
 
             b = (minChoiceDuration - maxChoiceDuration) / (numberOfLevels - 1);
             c = b * (currentLevel - 1) + maxChoiceDuration;
@@ -247,9 +258,11 @@ angular.module('App')
             score = scope.getScore();
             // console.log(score);
             if (is_correct) {
-                scope.userScore += score.potentialPoints;
+                scope.userScore = scope.userScore + score.potentialPoints;
+                return score.potentialPoints;
             } else {
-                scope.userScore += score.wrongPoints;
+                scope.userScore = scope.userScore + score.wrongPoints;
+                return score.wrongPoints;
             }
         }
 
